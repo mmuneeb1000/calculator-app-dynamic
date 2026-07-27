a3c595221094f0061e8dc99655b25526772a2430
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-27 21:43:52 +0500

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer
-      className="fixed text-sm text-key-text-inverse
-     bottom-2 bg-key-accent/70 rounded-lg px-2 py-1 hidden md:block"
+      className="text-sm text-key-text-inverse
+     bg-key-accent/70 rounded-lg mt-12 px-2 py-1 hidden md:block"
     >
       Challenge by
       <a
